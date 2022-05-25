a2b3dca9bc748af556c294a21c0ffd250020f2f5
Michael Palumbo <michaelpalumbo@users.noreply.github.com>
2022-05-25 14:54:13 -0400

    update

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // create our User model
 class User extends Model {}
@@ -45,6 +46,14 @@ User.init(
       }
     },
     {
+        hooks: {
+            // set up beforeCreate lifecycle "hook" functionality
+            beforeCreate(userData) {
+                return bcrypt.hash(userData.password, 10).then(newUserData => {
+                return newUserData
+                });
+            }
+        },
       sequelize,
       timestamps: false,
       freezeTableName: true,
